85ef5e15ce2c6f60d4e9b95e383e6c4de5eb88b3
joeljonesokecha <joeljonesokecha518@gmail.com>
2026-02-23 19:50:37 +0300

    Create products.js

---
+++ products/products.js
@@ -0,0 +1,17 @@
+const products = [
+
+{
+name:"Luxury Watch",
+price:350,
+image:"media/watch1.jpg",
+description:"Premium wrist fashion"
+},
+
+{
+name:"Classic Gold",
+price:420,
+image:"media/watch2.jpg",
+description:"Elegant modern style"
+}
+
+];

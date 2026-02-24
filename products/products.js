b9cf326f3def29320d64174e65948ea519abf5e0
wrist-vogue <joeljonesokecha518@gmail.com>
2026-02-24 21:36:32 +0300

    Update products.js

--- products/products.js
+++ products/products.js
@@ -1,16 +1,23 @@
 const products = [
 {
-    id: 1,
-    name: "Hublot Geneve",
-    price: 130000,
-    category: "men",
-    image: "assets/images/hublotgeneve.jpeg"
+  id: 1,
+  name: "Royal Gold Chrono",
+  price: 250,
+  category: "men",
+  images: [
+    "assets/images/royal1.webp",
+    "assets/images/royal2.webp",
+    "assets/images/royal3.webp"
+  ]
+},{
+  id: 1,
+  name: "Royal Gold Chrono",
+  price: 250,
+  category: "men",
+  images: [
+    "assets/images/royal1.webp",
+    "assets/images/royal2.webp",
+    "assets/images/royal3.webp"
+  ]
 },
-{
-    id: 2,
-    name: "Elegant Silver Queen",
-    price: 280,
-    category: "women",
-    image: "assets/images/watch2.jpg"
-}
 ];

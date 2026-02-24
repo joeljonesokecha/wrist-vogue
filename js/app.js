ea15bc163c5b79e2b68775f79d616b1fbb85727d
wrist-vogue <joeljonesokecha518@gmail.com>
2026-02-24 11:02:09 +0300

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const products = [
+{
+    id: 1,
+    name: "Royal Gold Chronograph",
+    price: 350,
+    category: "men",
+    image: "assets/images/watch1.jpg"
+},
+{
+    id: 2,
+    name: "Elegant Silver Queen",
+    price: 280,
+    category: "women",
+    image: "assets/images/watch2.jpg"
+}
+];

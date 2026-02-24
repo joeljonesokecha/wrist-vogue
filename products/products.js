d66d2438f05d047590963f4426f06cfc5470b7f2
wrist-vogue <joeljonesokecha518@gmail.com>
2026-02-24 22:57:27 +0300

    Update products.js

--- products/products.js
+++ products/products.js
@@ -1,13 +1,17 @@
 const products = [
 {
   id: 1,
-  name: "Audemars Piguet",
-  price: 150000,
+  name: "Hublot Geneve",
+  price: 130000,
   category: "men",
   images: [
-    "assets/images/image1.mp4",
-    "assets/images/royal2.webp",
-    "assets/images/royal3.webp"
+    "assets/images/hublotgeneve.jpeg",
+    "assets/images/hublotgeneve1.jpeg",
+    "assets/images/hublotgeneve2.jpeg"
+    "assets/images/hublotgeneve3.jpeg"
+    "assets/images/hublotgeneve4.jpeg"
+    "assets/images/hublotgeneve5.jpeg"
+    "assets/images/hublotgeneve6.jpeg"
   ]
 },{
   id: 1,
